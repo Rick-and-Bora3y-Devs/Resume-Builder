cb358c6c6eabb5be0495d57df7fcb353208e7dc5
shehab299 <shehab299@outlook.com>
2023-08-30 17:40:39 +0300

    resume builder

--- src/Pages/ResumeBuilder.jsx
+++ src/Pages/ResumeBuilder.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import styles from "../Css/Resume.module.css";
+import Template1 from "../Components/Templates/Template1.jsx";
+import Template2 from "../Components/Templates/Template2.jsx";
+import Template3 from "../Components/Templates/Template3.jsx";
+
+function ResumeBuilder()
+{
+
+    const [template, setTemplate] = useState(1);
 
 
+    return <div className={styles.Container}>
+        <div className={styles.Builder}>
 
-function ResumeBuilder(){
-    return (<>
-        <h1>Resume Builder</h1>
-    </>)
+        </div>
+        <div className={styles.Preview}>
+            <div className={styles.Controls}>
+                <button className={styles.Choose}>Choose Template</button>
+                <button className={styles.Download}>Download As PDF</button>
+            </div>
+            <div className={styles.resume}>
+                {template === 1 && <Template1/>}
+            </div>
+        </div>
+    </div>
+
+
 }
 
 
