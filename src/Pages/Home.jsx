2f58bfbe158c5c733979b6ee3847ca7969176a26
asmaa-204 <asmaa204211261@gmail.com>
2023-09-12 18:13:41 +0300

    nav bar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,8 +11,15 @@ function Home() {
                 <img className={Style.cover1} src={cover1} />
                 <img className={Style.cover2} src={cover2} />
             </div>
+
+            <div className={Style.navbar}>
+                <p className={Style.logo}>LOGO</p>
+                <div className={Style.links}>
+                    <Link to={"/login"}><button className={Style.login}>Login</button></Link>
+                    <Link to={"/signUp"}><button className={Style.signUp}>Sign Up</button></Link>
+                </div>
+            </div>
             
-            <p className={Style.logo}>LOGO</p>
             <div className={Style.content}>
                 <h1 className="hero-text">create a professional resume for free</h1>
                 <p>Land your dream job with the help of our resume builder</p>
